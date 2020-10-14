d8b0768074b89bf1a61d8acdb1288b070a7220a0
LindseyM20 <lindsey.bordner@gmail.com>
2020-10-14 16:22:41 -0500

    added switch statement for license badges

--- README.md
+++ README.md
@@ -1,21 +1,31 @@
-
-  # Test 3
+# Readme Generator
+  
+  [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
 
   ## Description
-  Look at that sweet arrow function and Ternary Operator!
+  A node program that will easily create a comprehensive, well-formatted Readme for your projects using your input
 
   ## Table of Contents
   * ### [Installation](#installation)
-  * [Usage](#usage)
-  * [License](#license)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
+  * ### [Usage](#usage)
+  * ### [License](#license)
+  * ### [Contributing](#contributing)
+  * ### [Tests](#tests)
+  * ### [Questions](#questions)
 
   ## Installation
-  ## Usage
-  ## License
-  ## Contributing
-  ## Tests
-  ## Questions
+  Commands to run to install dependencies: npm i
 
+  ## Usage
+  Clone this repo, save it locally, and run when you need a Readme
+
+  ## Contributing
+  Contact me
+
+  ## Tests
+  Command to run tests: npm test
+
+  ## Questions
+  Follow me on GitHub at https://github.com/LindseyM20
+
+  Please contact me with any questions or feedback at lindsey.bordner@gmail.com 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  # ${data.title}
+  var badge;
+  switch (data.license) {
+    case "APACHE 2.0":
+      badge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "MIT":
+      badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "GPL 3.0":
+      badge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "BSD 3":
+      badge = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+      break;
+    case "Unlicense":
+      badge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+      break;
+    case "none":
+      break;
+  }
+
+  return `# ${data.title}
+  
+  ${badge}
 
   ## Description
   ${data.description}
@@ -15,17 +37,20 @@ function generateMarkdown(data) {
   * ### [Questions](#questions)
 
   ## Installation
-  ${data.installation}
+  Commands to run to install dependencies: ${data.installation}
+
   ## Usage
   ${data.usage}
-  ## License
-  ${data.license}
+
   ## Contributing
   ${data.contributing}
+
   ## Tests
-  ${data.tests}
+  Command to run tests: ${data.test}
+
   ## Questions
-  Find me on GitHub at ${data.github}
+  Follow me on GitHub at https://github.com/${data.github}
+
   Please contact me with any questions or feedback at ${data.email} 
 `;
 }
